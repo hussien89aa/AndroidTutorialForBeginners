259dddd3638e2e712a05b1ce345b8d0eb6296fc6
hussienalrubaye <hussien89aa@yahoo.com>
2016-11-01 18:51:22 -0400

    alarm

--- AlarmManager.java
+++ AlarmManager.java
@@ -4,25 +4,27 @@
 Calendar myAlarmDate = Calendar.getInstance();
 myAlarmDate.setTimeInMillis(System.currentTimeMillis());
 myAlarmDate.set(2012, 11, 25, 12, 00, 0);
+//other way
+  Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, Hour);
+        calendar.set(Calendar.MINUTE, Minute);
+        calendar.set(Calendar.SECOND, 0);
 
- AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);  
-      
- Intent intent = new Intent(this, MyBroadcastReceiver.class); 
-intent.putExtra("MyMessage","HERE I AM PASSING THEPERTICULAR 
-        	MESSAGE WHICH SHOULD BE SHOW ON RECEIVER OF ALARM"); 
-PendingIntent pendingIntent = PendingIntent.getBroadcast(  
-                                      this.getApplicationContext(),
-                                       234324243, intent, 0);
-//start service
-  // PendingIntent pendingIntent = PendingIntent.getService(  
-                                      this.getApplicationContext(),
-                                       0, intent, 0);                                       
-alarmManager.set(AlarmManager.RTC_WAKEUP,
- myAlarmDate.getTimeInMillis(),_myPendingIntent); 
-/*  Create Repeating Alarm Start After Each 2 Minutes
-    am.setRepeating(AlarmManager.ELAPSED_REALTIME,myAlarmDate.getTimeInMillis(),
-                  2*60*60,pendingIntent);
- */
+ //define Repeating Alarm Start After Each 2 Minutes
+ 
+
+  AlarmManager am = (AlarmManager)context.getSystemService  (Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.setAction("com.quranonline.Broadcast");
+        intent.putExtra("MyMessage",context.getResources().getString(R.string.msg_notify));
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY , pi);
+// Create one time   Alarm Start After Each 2 Minutes
+alarmManager.set(AlarmManager.RTC_WAKEUP, myAlarmDate.getTimeInMillis(),_myPendingIntent); 
+
+ 
     }  
 
 
@@ -31,3 +33,15 @@ alarmManager.set(AlarmManager.RTC_WAKEUP,
 
 
     */
+
+       /* run with os
+       //permission
+           <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
+            
+            //filters
+             <action android:name="android.intent.action.BOOT_COMPLETED" />
+                 <action android:name="android.intent.action.ACTION_SHUTDOWN" />
+                <action android:name="android.intent.action.QUICKBOOT_POWEROFF" />
+
+
+       */
