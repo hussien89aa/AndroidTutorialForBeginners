7080981d7564e93a3287a143485ba1fbd7155f70
AnushkaSharma503 <59316156+AnushkaSharma503@users.noreply.github.com>
2020-10-17 13:43:09 +0530

    Update MainActivity.java
    Hello please review this and merge if you find that useful. Here I am adding a one line simple code for full screen. You can use that also.

--- Alaram App/app/src/main/java/com/alrubaye/alaram/MainActivity.java
+++ Alaram App/app/src/main/java/com/alrubaye/alaram/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         android.app.FragmentManager fm= getFragmentManager();
         PopTime popTime=new PopTime();
